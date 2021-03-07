ad2e754ca7f8079653704f801b90559f751f3bca
Florian Jam <jam.florian@gmail.com>
2021-03-07 21:45:44 +0100

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () =>
+  console.log("Connected to database")
+);
+
+mongoose.connection.on("error", () =>
+  console.log("Cannot connect to databse")
+);
