a68e9242d11c2af198a362ad0d3e7845f1495722
Florian Jam <jam.florian@gmail.com>
2021-03-11 19:52:17 +0100

    gimme error

--- app.js
+++ app.js
@@ -86,6 +86,15 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  console.log("----------------");
+  console.log("----------------");
+  console.log("----------------");
+  console.log("----------------");
+  console.log(err);
+  console.log("----------------");
+  console.log("----------------");
+  console.log("----------------");
+  console.log("----------------");
   // render the error page
   res.status(err.status || 500);
   res.render('error');
