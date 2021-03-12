a63e5d72bb94d9c1beee17893c2463e00d33b0d4
Florian Jam <jam.florian@gmail.com>
2021-03-12 11:58:54 +0100

    presentation version

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -2,33 +2,40 @@
 
 //SEARCH PRODUCT VARIABLES
 const inputSearchProducts = document.getElementById("search-input-products");
-const resultsContainerProducts = document.getElementById("results-container-products");
+const resultsContainerProducts = document.getElementById(
+  "results-container-products"
+);
 
 //COMMENT VARIABLES
 const sendBtn = document.getElementById("send-btn");
-const commentInput = document.getElementById("comment-input")
-const commentsContainer = document.getElementById("comments-container")
+const commentInput = document.getElementById("comment-input");
+const commentsContainer = document.getElementById("comments-container");
 
 //SEARCH USER VARIABLES
 const inputSearchUsers = document.getElementById("search-input-users");
-const resultsContainerUsers = document.getElementById("results-container-users");
+const resultsContainerUsers = document.getElementById(
+  "results-container-users"
+);
 
 // COMMENTS
 
 // DOM logic
 
 function publishComment(input) {
-  console.log('publishing input: ', input.content, input.author, input.date)
+  console.log("publishing input: ", input.content, input.author, input.date);
   commentsContainer.innerHTML += `<div>
   <div>${input.content}</div>
   <div>${input.author.firstName} ${input.author.lastName}</div>
-</div>`
+</div>`;
 }
 
 // AJAX handler
 function createCommentInDB(comment) {
-  console.log('new comment is : ', comment)
-  return axios.post(`/staff/product/${document.getElementById("product-id").innerHTML}`, comment)
+  console.log("new comment is : ", comment);
+  return axios.post(
+    `/staff/product/${document.getElementById("product-id").innerHTML}`,
+    comment
+  );
 }
 
 // DOM event handlers
@@ -36,84 +43,90 @@ function handleCreateComment(input, callback) {
   createCommentInDB({
     product: document.getElementById("product-id").innerHTML,
     content: input,
-    date: new Date()
+    date: new Date(),
   })
     .then((comment) => {
-        console.log("the comment in the db is :", comment.data)
-        callback(comment.data)
+      console.log("the comment in the db is :", comment.data);
+      callback(comment.data);
     })
     .catch((apiError) => console.log(apiError));
 }
 
 // DOM event listener
-window.addEventListener('load', () => {
-  if (sendBtn) sendBtn.addEventListener('click', () => {
-    console.log('clicked')
-    if(commentInput.value.length === 0) {
-      return
-    }
-    handleCreateComment(commentInput.value, publishComment)
-  })
-})
+window.addEventListener("load", () => {
+  if (sendBtn)
+    sendBtn.addEventListener("click", () => {
+      console.log("clicked");
+      if (commentInput.value.length === 0) {
+        return;
+      }
+      handleCreateComment(commentInput.value, publishComment);
+    });
+});
 
 //SEARCH PRODUCT
 // DOM logic
 function resetSearchResult() {
-    resultsContainerProducts.innerHTML = '';
+  resultsContainerProducts.innerHTML = "";
 }
 
 function displayResults(products) {
-    products.forEach((product) => {
-        resultsContainerProducts.innerHTML += `
+  products.forEach((product) => {
+    resultsContainerProducts.innerHTML += `
         <div>
                 <a href="/staff/product/${product._id}">
-                    <img src="${product.images[product.images.length - 1]}" alt="most recent image of product" class="product-image">
+                    <img src="${
+                      product.images[product.images.length - 1]
+                    }" alt="most recent image of product" class="product-image">
                 </a>
                 <div class="product-info">${product.name}</div>
-                <div class="product-info">${product.serie.season} ${product.serie.year}</div>
+                <div class="product-info">${product.serie.season} ${
+      product.serie.year
+    }</div>
 
-        </div>`
-    })
+        </div>`;
+  });
 }
 
 // AJAX handler
 function fetchProductByName(string) {
-    let query = string ? `?name=${string}` : "";
-    return axios.get(`/api/products${query}`);
-  }
+  let query = string ? `?name=${string}` : "";
+  return axios.get(`/api/products${query}`);
+}
 
 // DOM event handlers
 function handleRead(evt, callback) {
-    fetchProductByName(evt.target.value)
-      .then((apiRes) => {
-          console.log(apiRes.data);
-          callback(apiRes.data)
-      })
-      .catch((apiError) => console.log(apiError));
-  }
+  fetchProductByName(evt.target.value)
+    .then((apiRes) => {
+      console.log(apiRes.data);
+      callback(apiRes.data);
+    })
+    .catch((apiError) => console.log(apiError));
+}
 
 // DOM event listener
-if (inputSearchProducts) inputSearchProducts.onkeyup = (evt) => {
-    resetSearchResult()
+if (inputSearchProducts)
+  inputSearchProducts.onkeyup = (evt) => {
+    resetSearchResult();
     handleRead(evt, displayResults);
   };
 
-  //SEARCH USER
+//SEARCH USER
 
 function displayResultsUsers(users) {
   users.forEach((user) => {
-      resultsContainerUsers.innerHTML += `
+    resultsContainerUsers.innerHTML += `
       <div>
           <a href="/staff/users/${user._id}">
           ${user.firstName} ${user.lastName}
           </a>
-      </div>`
-  })
+      </div>`;
+  });
 }
 
 //DOM LOGIC
 function resetSearchResultUsers() {
-  resultsContainerUsers.innerHTML = '';
+  resultsContainerUsers.innerHTML = "";
 }
 
 // AJAX handler
@@ -126,16 +139,15 @@ function fetchUserByName(string) {
 function handleReadUserSearch(evt, callback) {
   fetchUserByName(evt.target.value)
     .then((apiRes) => {
-        console.log(apiRes.data);
-        callback(apiRes.data)
+      console.log(apiRes.data);
+      callback(apiRes.data);
     })
     .catch((apiError) => console.log(apiError));
 }
 
 // DOM event listener
-if (inputSearchUsers) inputSearchUsers.onkeyup = (evt) => {
-  resetSearchResultUsers()
-  handleReadUserSearch(evt, displayResultsUsers);
-};
-
-
+if (inputSearchUsers)
+  inputSearchUsers.onkeyup = (evt) => {
+    resetSearchResultUsers();
+    handleReadUserSearch(evt, displayResultsUsers);
+  };
